45b71c5d10a202f2b0ae62bfc4d5cd3114996c8d
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-03-09 13:09:01 +0100

    Update index.js

--- src/services/files/index.js
+++ src/services/files/index.js
@@ -21,12 +21,6 @@ const writeAuthors = content => fs.writeFileSync(authorsJSONPath, JSON.stringify
 
 const filesRouter = express.Router()
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.API_KEY, 
-  api_secret: process.env.API_SECRET,
-  secure: true
-})
 
 const cloudStorageAut = new CloudinaryStorage({
   cloudinary,
